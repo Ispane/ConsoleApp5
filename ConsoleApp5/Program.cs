6311304e76a8a64bbf49c1cbdcdb4b4c40f659de
Admin <Admin@DESKTOP-9H2KH00>
2022-03-03 15:56:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Timer timer = new Timer();
+                Clock clock = new Clock();
+
+                timer.TimeChangeEvent += clock.ParsecTime;
+
+                timer.Start();
+            }
+        }
+    
+}
+
